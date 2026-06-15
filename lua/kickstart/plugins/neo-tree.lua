1dbe50a32f632265833be7c32ec55417047850e1
mengstabketemaw <mengstabrealk28@gmail.com>
2026-06-15 14:20:29 +0300

    change tree to vim tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -5,26 +5,17 @@ local plugins = {
   { src = 'https://github.com/nvim-tree/nvim-tree.lua', version = vim.version.range '*' },
 }
 
-if vim.g.have_nerd_font then
-  table.insert(plugins, 'https://github.com/nvim-tree/nvim-web-devicons')
-end
+if vim.g.have_nerd_font then table.insert(plugins, 'https://github.com/nvim-tree/nvim-web-devicons') end
 
 vim.pack.add(plugins)
 
 vim.keymap.set('n', '\\', '<Cmd>NvimTreeToggle<CR>', { desc = 'NvimTree toggle', silent = true })
 
-require('nvim-tree').setup {
-  disable_netrw = true,
-  filters = {
-    dotfiles = false,
-    git_ignored = false,
-  },
-  view = {
-    width = 30,
-  },
-  actions = {
-    open_file = {
-      quit_on_open = true,
-    },
-  },
-}
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup()
+
